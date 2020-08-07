bf662878bd3c3225ca30f493ff8b2265453385cd
Thai Nguyen Hung <hungthai1401.it@gmail.com>
2020-08-07 14:44:56 +0700

    move promise to async await

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import Vue from 'vue';
 import { CoreModule } from '@/modules/core';
 
-async function bootstrap() {
+function bootstrap() {
   Vue.config.productionTip = false;
   const coreModule = new CoreModule();
   coreModule.install(Vue);
   const requireModules = require.context('./modules', true, /\module.ts$/);
-  requireModules.keys().forEach((module) => {
+  requireModules.keys().forEach(async (module) => {
     if (module.includes('core')) {
       return;
     }
     const moduleName = module.replace(/(\.\/|\.ts)/g, '');
-    import(`@/modules/${moduleName}`).then((moduleImported) => {
-      const instanceModule = new moduleImported.Module(coreModule.router!, coreModule.store!);
-      instanceModule.install(Vue);
-    });
+    const moduleImported = await import(`@/modules/${moduleName}`);
+    const instanceModule = new moduleImported.Module(coreModule.router!, coreModule.store!);
+    instanceModule.install(Vue);
   });
 }
 
